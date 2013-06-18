8ca423d63352ff3e182b47a02472ab168d0b9964
Jason Peak <jpeak5@lsu.edu>
2013-06-18 11:41:20 -0500

    adds capability myaddinstance, bumps version

---
+++ db/access.php
@@ -0,0 +1,26 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+//    'block/my_picture:addinstance' => array(
+//        'captype' => 'write',
+//        'contextlevel' => CONTEXT_SYSTEM,
+//        'archetypes' => array(
+//            'student' => CAP_ALLOW
+//        ),
+//
+//        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+//    ),
+    
+    'block/grades_at_a_glance:myaddinstance' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'student' => CAP_ALLOW
+        ),
+ 
+        'clonepermissionsfrom' => 'moodle/my:manageblocks'
+    ),
+);
+?>
