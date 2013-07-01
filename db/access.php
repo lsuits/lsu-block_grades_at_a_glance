07dd156642f93c5b734070b82576064ebd092aab
Jason Peak <jpeak5@lsu.edu>
2013-07-01 13:51:12 -0500

    adds lang strings to match caps

--- db/access.php
+++ db/access.php
@@ -3,15 +3,15 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-//    'block/my_picture:addinstance' => array(
-//        'captype' => 'write',
-//        'contextlevel' => CONTEXT_SYSTEM,
-//        'archetypes' => array(
-//            'student' => CAP_ALLOW
-//        ),
-//
-//        'clonepermissionsfrom' => 'moodle/site:manageblocks'
-//    ),
+    'block/grades_at_a_glance:addinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => array(
+            'student' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
     
     'block/grades_at_a_glance:myaddinstance' => array(
         'captype' => 'read',
--- lang/en/block_grades_at_a_glance.php
+++ lang/en/block_grades_at_a_glance.php
@@ -3,3 +3,5 @@
 $string['pluginname'] = 'Grades at a Glance';
 $string['hidden'] = 'Hidden';
 $string['no_courses'] = 'No courses as student';
+$string['grades_at_a_glance:myaddinstance'] = 'Add Grades at a Glance Block to the My page';
+$string['grades_at_a_glance:addinstance'] = 'Add Grades at a Glance Block';
