1337593e0c1f0558bbcdbfe1d7f5ae3ae3841158
Robert Russo <rrusso@lsu.edu>
2017-10-12 10:20:56 -0500

    Add copyright headings to pages

--- version.php
+++ version.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -14,6 +13,13 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    block_grades_at_a_glance
+ * @copyright  2008-2017 Louisiana State University
+ * @copyright  2008-2017 Adam Zapletal, Jason Peak, Chad Mazilly, Philip Cali, Robert Russo
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $plugin->version = 2013062708;
 $plugin->component = 'block_grades_at_a_glance';
